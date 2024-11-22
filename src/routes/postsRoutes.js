ed3d796ceeb1c548387c3ad7085ac92a08162b29
rianfernandesno <zrpapz12@gmail.com>
2024-11-22 09:04:30 -0300

    first comit

---
+++ src/routes/postsRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import multer from "multer";
+import { listarPosts, postaNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage})
+//linux ou no mac
+// const upload = multer({ dest: ".uploads"})
+const routes = (app) => {
+    app.use(express.json());
+    
+    app.get("/posts", listarPosts);
+    app.post("/posts", postaNovoPost);
+    app.post("/upload", upload.single("imagem"), uploadImagem )
+}
+
+export default routes;
